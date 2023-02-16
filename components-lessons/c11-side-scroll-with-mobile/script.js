1af699b143746151293c7f2f0deb4d6c397b4019
Jax I <jaxpi85@gmail.com>
2023-02-16 12:14:10 -0500

    added classes and functions to be used in script.js

--- components-lessons/c11-side-scroll-with-mobile/script.js
+++ components-lessons/c11-side-scroll-with-mobile/script.js
@@ -2,4 +2,47 @@
 document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 720;
+
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      window.addEventListener('keydown', function (e) {
+        console.log(e);
+      })
+  }
+  }
+
+  class Player {
+    constructor() {
+    
+    }
+  }
+
+  class Background {
+    constructor() {
+    
+    }
+  }
+
+  class Enemy {
+    constructor() {
+    
+    }
+  }
+
+  function handleEnemies() {
+    
+  }
+
+  function displayStatusText(){
+
+  }
+
+  const input = new InputHandler();
+
+  function animate(){
+
+  }
 });
