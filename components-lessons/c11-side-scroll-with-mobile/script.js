a1f967596a952b49bb269614dd617170c57a0114
Jax I <jaxpi85@gmail.com>
2023-02-16 11:58:31 -0500

    beginning c11 with new files

---
+++ components-lessons/c11-side-scroll-with-mobile/script.js
@@ -0,0 +1,5 @@
+/** @type {HTMLCanvasElement} */
+document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+})
