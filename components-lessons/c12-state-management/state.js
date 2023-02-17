b13fa603f725a5fe84f49d5d5fe83293c98fbd08
Jax I <jaxpi85@gmail.com>
2023-02-17 15:19:49 -0500

    added codes for player sitting and standing on key presses in both directions

--- components-lessons/c12-state-management/state.js
+++ components-lessons/c12-state-management/state.js
@@ -1,6 +1,8 @@
 export const states = {
   STANDING_LEFT: 0,
   STANDING_RIGHT: 1,
+  SITTING_LEFT: 2,
+  SITTING_RIGHT: 3,
 };
 
 class State {
@@ -11,26 +13,56 @@ class State {
 
 export class StandingLeft extends State {
   constructor(player) {
-    super('STANDING LEFT');
+    super("STANDING LEFT");
     this.player = player;
   }
   enter() {
     this.player.frameY = 1;
   }
   handleInput(input) {
-    if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT);
+      if (input === "PRESS right") this.player.setState(states.STANDING_RIGHT);
+      else if (input === 'PRESS down') this.player.setState(states.SITTING_LEFT);
   }
 }
 
 export class StandingRight extends State {
   constructor(player) {
-    super('STANDING RIGHT');
+    super("STANDING RIGHT");
     this.player = player;
   }
   enter() {
     this.player.frameY = 0;
   }
   handleInput(input) {
-    if (input === 'PRESS left') this.player.setState(states.STANDING_LEFT);
+      if (input === "PRESS left") this.player.setState(states.STANDING_LEFT);
+      else if (input === 'PRESS down') this.player.setState(states.SITTING_RIGHT);
   }
 }
+
+export class SittingLeft extends State {
+  constructor(player) {
+    super("SITTING LEFT");
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 9;
+  }
+  handleInput(input) {
+    if (input === "PRESS right") this.player.setState(states.SITTING_RIGHT);
+      else if (input === "RELEASE down") this.player.setState(states.STANDING_LEFT);
+  }
+}
+
+export class SittingRight extends State {
+    constructor(player) {
+      super("SITTING RIGHT");
+      this.player = player;
+    }
+    enter() {
+      this.player.frameY = 8;
+    }
+    handleInput(input) {
+      if (input === "PRESS left") this.player.setState(states.SITTING_LEFT);
+        else if (input === "RELEASE down") this.player.setState(states.STANDING_RIGHT);
+    }
+  }
